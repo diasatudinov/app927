eb987fe48e59df741888e11bf56203dda89ff1ca
Dias <dias.atudinov@gmail.com>
2024-09-19 13:05:46 +0500

    create folder for app

---
+++ app927/app927/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app927
+//
+//  Created by Dias Atudinov on 19.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app927/app927/app927App.swift
@@ -0,0 +1,17 @@
+//
+//  app927App.swift
+//  app927
+//
+//  Created by Dias Atudinov on 19.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct app927App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
