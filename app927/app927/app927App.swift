f4fce0934991ca9488a3d47c0cbfdacec3983db6
Dias <dias.atudinov@gmail.com>
2024-09-19 16:25:41 +0500

    app launch setup

--- app927/app927/app927App.swift
+++ app927/app927/app927App.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct app927App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                LoaderUIView()
+            }
         }
     }
 }
